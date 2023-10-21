0fbe34cc5b5bbbf2866a6e71965a02f3e65c61ce
yigit <mehmetyigityalim@gmail.com>
2023-10-21 23:57:20 +0300

    proje yapısı oluşturuldu

---
+++ src/main/java/com/yigit/countriesbackend/CountriesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yigit.countriesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yigit/countriesbackend/CountriesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yigit.countriesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
